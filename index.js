893a09ed651906d0519d565901a40b2f84878dc7
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-26 13:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 findMatching(drivers, name){
-  return drivers.filter(function driverName){
+  return drivers.filter(function (driverName){
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
