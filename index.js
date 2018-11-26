336de305a3f040fc3033332254251679a8280bdc
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-26 13:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 
-findMatching(drivers, name) {
+function findMatching(drivers, name) {
   return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
