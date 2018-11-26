889bfa93ad094e5d3e57192bd1abb5e8fe225b8d
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-26 13:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Code your solution in this file
 
-findMatching(drivers)
+findMatching(drivers, name){
+  return drivers.filter(function driverName){
+    return driverName.toLowerCase() === name.toLowerCase();
+  }
+}
 
 fuzzyMatch()
 
